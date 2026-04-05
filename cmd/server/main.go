e90a6eaf455228ee014fcd0efbcd4ba249e47287
Dakota Lillie <dakota.lillie@icloud.com>
2026-04-05 08:01:58 -0700

    Refactor

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
-	"github.com/dakotalillie/rota/internal/api"
+	"github.com/dakotalillie/rota/internal/application"
 	"github.com/dakotalillie/rota/internal/config"
 	"github.com/dakotalillie/rota/internal/infrastructure/sqlite"
+	"github.com/dakotalillie/rota/internal/presentation"
 )
 
 func main() {
@@ -23,10 +29,33 @@ func main() {
 	}
 	defer db.Close()
 
-	api := api.New(conf, db)
+	var (
+		rotationRepo       = sqlite.NewRotationRepository(db)
+		getRotationUseCase = application.NewGetRotationUseCase(rotationRepo)
+		getRotationHandler = presentation.NewGetRotationHandler(conf.Hostname, getRotationUseCase.Execute)
+	)
 
-	if err := api.Start(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error starting API: %v\n", err)
-		os.Exit(1)
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /api/rotations/{rotationID}", getRotationHandler.Handle)
+
+	server := &http.Server{Addr: ":8080", Handler: mux}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	errCh := make(chan error, 1)
+	go func() { errCh <- server.ListenAndServe() }()
+
+	select {
+	case <-quit:
+		if err := server.Shutdown(context.Background()); err != nil {
+			fmt.Fprintf(os.Stderr, "Error shutting down server: %v\n", err)
+			os.Exit(1)
+		}
+	case err := <-errCh:
+		if !errors.Is(err, http.ErrServerClosed) {
+			fmt.Fprintf(os.Stderr, "Error running server: %v\n", err)
+			os.Exit(1)
+		}
 	}
 }
